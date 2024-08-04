7f95cc7c518d810a3ab35305f800bd68d7fb34ad
Om Panpatil <139043319+OmPanpatil@users.noreply.github.com>
2024-08-04 21:23:54 +0530

    N days of code

---
+++ index.js
@@ -0,0 +1,60 @@
+// let firstname ="Om";
+// const age = 30;
+// var isStudent = true;
+
+// console.log(firstname);
+// console.log(age);
+// console.log(isStudent);
+
+// Function declaration
+// function greet(name){
+//     return "Hello, " + name;
+// }
+
+// Function calls
+
+// let ans = greet("Om");
+// let ans2 = greet("Sahil");
+
+// console.log(ans);
+// console.log(ans2);
+
+
+// Assignment-1
+
+// let favouritecolor = "Blue";
+// let Height = "21";
+// var isPizza = true;
+
+// console.log(favouritecolor);
+// console.log(Height);
+// console.log(isPizza);
+
+// Assignment-2
+
+// Function declaration
+// function sum(a,b) {
+//     let totalsum = a+b;
+//     return totalsum;
+// }
+
+// Function call
+// let ans = sum(3,6);
+// let ans2 = sum(4,5)
+
+// We can't write a string instead of numbers it gives a reference error as not defined
+
+// console.log(ans);
+// console.log(ans2);
+
+// Assignment-3
+
+let numbers = 65;
+
+if (numbers %2 == 0) {
+    console.log("The number is Even.");
+} else {
+    console.log("The number is Odd");
+}
+
+
